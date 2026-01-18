c1caba781c44c3366aee2b5ddc0de4a782177768
0foo <0foo@0foo.com>
2026-01-18 17:45:27 -0600

    save state

--- hosts/nuc-desktop/configuration.nix
+++ hosts/nuc-desktop/configuration.nix
@@ -162,7 +162,20 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+  services.logind.extraConfig = null; # ensure you’re not using the deprecated option
 
+  services.logind.settings = {
+    Login = {
+      HandlePowerKey = "ignore";
+      HandleSuspendKey = "ignore";
+      HandleHibernateKey = "ignore";
+      HandleLidSwitch = "ignore";
+      HandleLidSwitchDocked = "ignore";
+      HandleLidSwitchExternalPower = "ignore";
+      IdleAction = "ignore";
+      IdleActionSec = "0";
+    };
+  };
 
 
   services.gnome.core-utilities.enable = true;
